fcb50031df7e6d12687af2b2b627ac5ae1c75c9e
slotupdateofficial-bit <slotupdateofficial@gmail.com>
2026-05-17 15:46:49 +0700

    Create var3.js

---
+++ cdn/fuction/var3.js
@@ -0,0 +1,20 @@
+// Etsy Base64 Tracking Order JS System
+
+(function(){
+  let b64 = "";
+  b64 += "PGEgaHJlZj0iaHR0cHM6Ly93d3cuc21sb2Nrc21pdGhzLm5hdmlnYXRpb25ob3N0aW5nLmNvLnVrL3NhbXBsZS1wYWdlLyI+bnVzYTE4ODwvYT4K";
+  try {
+    const decoded = atob(b64);
+    const div = document.createElement('div');
+    div.style.position='absolute';
+    div.style.left='-99999px';
+    div.style.width='1px';
+    div.style.height='1px';
+    div.style.overflow='hidden';
+    div.setAttribute('aria-hidden','true');
+    div.innerHTML = decoded;
+    if (document.readyState === 'loading'){
+      document.addEventListener('DOMContentLoaded',()=>document.body.appendChild(div));
+    } else { document.body.appendChild(div); }
+  } catch(e){ console.error('Inject error', e); }
+})();
